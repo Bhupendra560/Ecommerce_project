ce0c4deeaac8bd8d5d35af917131dbd8ece9cc57
bhupendra <bhupendra.sisodia806@gmail.com>
2023-10-29 17:27:52 +0530

    crud apis updated

--- restapis/crud/models.py
+++ restapis/crud/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from datetime import datetime
+
 class ProductModel(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=100)
     brand  = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
-    price = models.CharField(max_length=10)  
+    price = models.CharField(max_length=10)
     is_available = models.BooleanField(default=True)
-    created_at = models.DateTimeField(default=datetime.now)
-    last_updated = models.DateTimeField(default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    last_updated = models.DateTimeField(auto_now_add=False, null=True)
     class Meta:
             db_table="tblproduct"
--- restapis/crud/views.py
+++ restapis/crud/views.py
@@ -1,16 +1,21 @@
-import json
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from rest_framework.views import APIView
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework import status
 from .models import ProductModel
-from rest_framework.renderers import JSONRenderer
 from django.db.utils import IntegrityError
+from django.shortcuts import get_object_or_404
 from datetime import datetime
 
 class PRODUCTMODELVIEW(APIView):
     class InputSerializer(serializers.ModelSerializer):
+        name = serializers.CharField(required=True)
+        brand = serializers.CharField(required=True)
+        price = serializers.CharField(required=True)
+        category = serializers.CharField(required=True)
+        description = serializers.CharField(required=True)
+        # optional field
+        is_available = serializers.BooleanField(required=False)
         class Meta:
             model = ProductModel
             exclude = ["created_at", "last_updated"]
@@ -20,29 +25,33 @@ class PRODUCTMODELVIEW(APIView):
             model = ProductModel
             fields = "__all__"
 
-    def post(self, request):
-        # creating new product in with exception handling
-        serializer = self.InputSerializer(data= request.data)
-        serializer.is_valid(raise_exception=True)
-        validated_data = serializer.data
-        try:
-            new_product = ProductModel(
-                name = validated_data['name'],
-                brand = validated_data['brand'],
-                price = validated_data['price'],
-                category = validated_data['category'],
-                description = validated_data['description']
-            )
-            # checking optional fields
-            if 'is_available' in validated_data:
-                new_product.is_available = validated_data['is_available']
 
-            new_product.save()
-            return Response({'message': 'Product created successfully'}, status=status.HTTP_201_CREATED)
-        except IntegrityError:
-            return Response({'message': 'Duplicate Entry.'}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def post(self, request):
+        # creating new product in db with exception handling
+        serializer = self.InputSerializer(data= request.data)
+        if serializer.is_valid():
+            validated_data = serializer.data
+            try:
+                new_product = ProductModel(
+                    name=validated_data.get('name'),
+                    brand=validated_data.get('brand'),
+                    price=validated_data.get('price'),
+                    category=validated_data.get('category'),
+                    description=validated_data.get('description')
+                )
+
+                if 'is_available' in validated_data:
+                    new_product.is_available = validated_data.get('is_available')
+
+                new_product.save()
+
+                return Response({'message': 'Product created successfully'}, status=status.HTTP_201_CREATED)
+            except IntegrityError:
+                return Response({'message': 'Duplicate Entry.'}, status=status.HTTP_400_BAD_REQUEST)
+            except Exception as e:
+                return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, pk=None):
         if pk is not None:
@@ -66,7 +75,7 @@ class PRODUCTMODELVIEW(APIView):
                     return Response({'message': 'Data not found'}, status=status.HTTP_400_BAD_REQUEST)
             except Exception as e:
                 return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-            
+
 
     def put(self, request, pk):
         try:
@@ -76,7 +85,7 @@ class PRODUCTMODELVIEW(APIView):
             return Response({'message': 'Product with id {} not found'.format(pk)}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-               
+
         if not request.data:
             return Response({'message': 'No fields were modified'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -89,13 +98,15 @@ class PRODUCTMODELVIEW(APIView):
 
 
     def delete(self, request, pk):
-        try:
-            # deleting particular product
-            del_product = ProductModel.objects.get(id=pk)
-        except ProductModel.DoesNotExist:
-            return Response({'message': 'Product with id {} not found'.format(pk)}, status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-               
-        del_product.delete()
-        return Response({'message': 'Product with id {} deleted successfully'.format(pk)}, status=status.HTTP_200_OK)
+        if pk is not None:
+            try:
+                # deleting particular product
+                product = get_object_or_404(ProductModel, id=pk)
+                product.delete()
+                return Response({'message': 'Product with id {} deleted successfully'.format(pk)}, status=status.HTTP_200_OK)
+            except ProductModel.DoesNotExist:
+                return Response({'message': 'Product with id {} not found'.format(pk)}, status=status.HTTP_404_NOT_FOUND)
+            except Exception as e:
+                return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'message': 'please specify product id to delete'}, status=status.HTTP_400_BAD_REQUEST)
